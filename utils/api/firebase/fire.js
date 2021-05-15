55e46cab9e60eaf843b9be6a03de86ba4dbfd927
Bhanu Pratap Singh <bhanuparitapsingh09@gmail.com>
2021-05-15 20:11:35 +0530

    adding string formating encoding

--- utils/api/firebase/fire.js
+++ utils/api/firebase/fire.js
@@ -1,7 +1,7 @@
 import { readFileSync } from "fs";
 import admin from "firebase-admin";
 
-const firebaseCredentials = JSON.parse(readFileSync("firebase.json"));
+const firebaseCredentials = JSON.parse(readFileSync("firebase.json", "utf8"));
 
 admin.initializeApp({
     credential: admin.credential.cert(firebaseCredentials),
